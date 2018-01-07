917eb04c850ab348de3d6f21e8e9ce515264e7f6
jakubsadowski08 <33435745+jakubsadowski08@users.noreply.github.com>
2018-01-07 14:06:17 +0000

    Update lista.c

--- lista.c
+++ lista.c
@@ -9,7 +9,7 @@ struct sedzia
 struct wezel
 {
 	struct sedzia * Sedzia;
-	struct sedzia * next;
+	struct wezel * next;
 };
 void dodaj(struct wezel * head,struct wezel * element)
 {
@@ -26,17 +26,22 @@ void dodaj(struct wezel * head,struct wezel * element)
 }
 void wczytaj(char nazwiskos[256], int ids, struct wezel * head)
 {
+	
 	struct wezel * element = (struct wezel *)malloc(sizeof(struct wezel));
 	strcpy(element->Sedzia->nazwisko,nazwiskos);
 	element->Sedzia->id = ids;
+	if(head->next == NULL)
+	{
+		element->next = NULL;
+		head = element;
+	}
 	dodaj(head, element);
 }
 int main(int argc, char const *argv[])
 {
 	struct wezel lista;
 	lista.Sedzia = NULL;
-	int ns;
-	scanf("%d",&ns);
+	lista.next = NULL;
 	wczytaj("Jacek",1,&lista);
 	return 0;
 }
