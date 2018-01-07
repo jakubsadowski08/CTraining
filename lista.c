0e7a87efc1a50dcbffaff5a05ba6d1458f92aafc
jakubsadowski08 <33435745+jakubsadowski08@users.noreply.github.com>
2018-01-07 22:14:07 +0000

    Update lista.c

--- lista.c
+++ lista.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <time.h>
 struct Sedzia
 {
     char nazwisko[256];
@@ -37,22 +37,77 @@ void przydziel(char nazwiskos[256],int ids,struct Wezel * head)
     element->dane->id = ids;
     dodaj(head, element);
 }
-void wypisz(struct Wezel head)
+int czyBylaWylosowana( int iLiczba, int tab[], int ile )
 {
-    struct Wezel * tmp = &head;
-    while (tmp != NULL)
+    if( ile <= 0 )
+        return 0;
+
+    int i = 0;
+    do
     {
-        printf("%s %d\n",tmp->dane->nazwisko,tmp->dane->id);
+        if( tab[ i ] == iLiczba )
+            return 1;
+        i++;
+    } while( i < ile );
+
+    return 0;
+}
+int losuj(int ile)
+{
+    return 1+ rand()%(ile*(ile-1)/2+1);
+}
+struct  Sedzia losowy(struct Wezel head, int los)
+{
+    int i;
+    struct Wezel * tmp = &head;
+    for(i=1;i<los;i++)
+    {
         tmp = tmp->next;
     }
+    return * tmp->dane;
+}
+void wypisz(struct Wezel * head,int nd)
+{
+    int i;
+    struct Sedzia sendzia;
+    int wylosowane[ nd*(nd-1)/2 ];
+    int wylosowanych = 0;
+    do
+    {
+        int liczba = losuj(nd);
+        if( czyBylaWylosowana( liczba, wylosowane, wylosowanych ) == 0)
+        {
+            wylosowane[ wylosowanych ] = liczba;
+            wylosowanych++;
+        }
+    } while( wylosowanych <= nd*(nd-1)/2 );
+    for(i=0;i<=nd*(nd-1)/2;i++)
+    {
+        sendzia = losowy(*head,wylosowane[i]);
+        printf("Gra nr %d - Sedzia %s o id %d \n",i+1,sendzia.nazwisko,sendzia.id);
+    }
+
+
 }
 int main() {
-    struct Wezel head;
-    head.next = NULL;
-    head.dane = NULL;
-    przydziel("Sad",1,& head);
-    przydziel("Sadowski",2,& head);
-    przydziel("Sads",3,& head);
-    wypisz(head);
+    time_t tt;
+    int zarodek = time(&tt);
+    srand(zarodek);
+    struct Wezel * head;
+    int nd, i, id;
+    char nazwisko[256];
+    head->next = NULL;
+    head->dane = NULL;
+    printf("Podaj liczbe druzyn ");
+    scanf("%d",&nd);
+    for(i=0;i<=nd*(nd-1)/2;i++)
+    {
+        printf("Podaj nazwisko sedziego: ");
+        scanf("%s",nazwisko);
+        printf("Podaj id sedziego: ");
+        scanf("%d", &id);
+        przydziel(nazwisko,id,head);
+    }
+    wypisz(head,nd);
     return 0;
 }
